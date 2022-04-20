50d61553d2371482b9f7a9976265e03ec9ee91da
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-04-20 10:54:15 +0200

    ultimo paso

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+const initialCart = [
+  {id:1, title: "Product", description: "Desc"},
+  {id:2, title: "Product #2", description: "Desc #2"}
+];
 
 const ShoppingCart = () => {
+
+  const [cart, setCart] = useState (initialCart);
+
   return (
     <div>ShoppingCart</div>
   )
