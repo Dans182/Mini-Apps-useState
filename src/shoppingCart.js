bc71e4c3e5ca9b620131af7faf69b1c3e08edf9b
Daniel Gaiteiro <dgaiteiro@gmail.com>
2022-04-21 12:50:42 +0200

    5

--- src/shoppingCart.js
+++ src/shoppingCart.js
@@ -1,19 +1,23 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const initialCart = [
-  {id:1, title: "Product", description: "Desc"},
-  {id:2, title: "Product #2", description: "Desc #2"}
+  { id: 1, title: "Product", description: "Desc" },
+  { id: 2, title: "Product #2", description: "Desc #2" },
 ];
 
 const ShoppingCart = () => {
-
-  const [cart, setCart] = useState (initialCart);
+  const [cart, setCart] = useState(initialCart);
 
   return (
     <div>
-      ShoppingCart
+      {cart.map((product) => (
+        <div key={product.id}>
+          <h1>{product.title}</h1>
+          <p>{product.description}</p>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ShoppingCart
+export default ShoppingCart;
